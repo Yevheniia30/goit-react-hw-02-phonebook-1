9d7282b326bac3e85d8dd2692fe5f9642ab4a1bd
Evgeniya3010 <susskaya86@gmail.com>
2021-03-04 01:17:08 +0200

    step 1-2 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import shortid from 'shortid';
 // import s from './App.module.css';
 import Form from './Components/Form';
-// import ContactsList from './Components/ContactsList';
+import ContactsList from './Components/ContactsList';
 
 class App extends Component {
   state = {
@@ -11,15 +12,26 @@ class App extends Component {
   };
 
   // ф-ция для получения данных введенных в форму
-  formOnSubmitHandler = data => {
-    console.log(data);
+  formOnSubmitHandler = ({ name, number }) => {
+    // console.log(data);
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    // добавляем каждый введенный контакт в массив
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
   };
 
   render() {
+    const { contacts } = this.state;
     return (
       <div className="App-main">
         {/* пропс onSubmit */}
         <Form onSubmit={this.formOnSubmitHandler} />
+        <ContactsList contacts={contacts} />
       </div>
     );
   }
--- src/Components/ContactsList/ContactsList.jsx
+++ src/Components/ContactsList/ContactsList.jsx
@@ -1,2 +1,16 @@
 // import React, { Component } from 'react';
 // import s from './ContactsList.module.css';
+
+const ContactsList = ({ contacts }) => {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name}: {contact.number}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactsList;
